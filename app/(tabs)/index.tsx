bb368574746b6f92d2e5d837e7acf28a53ad5503
Mohit Raut <mohitraut.21@stvincentngp.edu.in>
2025-08-03 22:53:54 +0530

    feat: enhance movie listing with loading and error handling in Index component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,19 @@
-import { Text, View,Image, ScrollView } from "react-native";
+import { Text, View,Image, ScrollView, ActivityIndicator, FlatList } from "react-native";
 import { images } from "@/constants/images";
 import { icons } from "@/constants/icons";
 import SearchBar from "@/components/SearchBar";
 import { useRouter } from 'expo-router';
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
 
 export default function Index() {
-    const router = useRouter();
+  const router = useRouter();
+
+  const { 
+    data: movies, 
+    loading: loadingMovies, 
+    error: moviesError 
+  } = useFetch(() => fetchMovies({ query: '' }));
   return (
     <View
       className="flex-1 bg-primary"
@@ -19,10 +27,30 @@ export default function Index() {
             paddingBottom: 10,
           }}>
         <Image source={icons.logo} className="w-12 h-10 mx-auto mt-20 mb-5" />
-        <View className="mb-5 flex-1">
-          <SearchBar onPress={() => router.push('/search')} 
-            placeholder="Search for movies..."/>
-        </View>
+        {loadingMovies ? (
+          <ActivityIndicator size={"large"} color="#0000ff" className="mt-10 self-center" />
+        ): moviesError ? (
+          <Text >
+            Error: {moviesError?.message}
+          </Text>
+        ) : (
+            <View className="mb-5 flex-1">
+              <SearchBar onPress={() => router.push('/search')} 
+              placeholder="Search for movies..."/>
+              <>
+                <Text className="text-white font-bold mt-5 mb-3 text-lg">
+                  Popular Movies
+                </Text>
+                <FlatList 
+                  data={movies}
+                  renderItem={({item}) => (
+                    <Text className="text-white text-sm">{item.title}</Text>
+                  )}
+                />
+              </>
+            </View>
+        )}
+
         </ScrollView>
     </View>
   );
