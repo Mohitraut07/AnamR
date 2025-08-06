5504411be2d89904c783ddea82c78e907fe37e38
Mohit Raut <mohitraut.21@stvincentngp.edu.in>
2025-08-06 23:58:43 +0530

    feat: add trending movies feature with TrendingCard component and update search count logic

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,15 +8,24 @@ import useFetch from "@/services/useFetch";
 import { fetchMovies } from "@/services/api";
 import SearchBar from "@/components/SearchBar";
 import MovieCard from "@/components/MovieCard";
+import { getTrendingMovies } from "@/services/appwrite";
+import TrendingCard from "@/components/TrendingCard";
 
 export default function Index() {
   const router = useRouter();
+
+  const {
+    data: trendingMovies, 
+    loading: loadingTrendingMovies, 
+    error: trendingMoviesError, 
+  } = useFetch(getTrendingMovies);
 
   const { 
     data: movies, 
     loading: loadingMovies, 
     error: moviesError, 
   } = useFetch(() => fetchMovies({ query: '' }));
+
   return (
     <View
       className="flex-1 bg-primary"
@@ -30,16 +39,38 @@ export default function Index() {
             paddingBottom: 10,
           }}>
         <Image source={icons.logo} className="w-12 h-10 mx-auto mt-20 mb-5" />
-        {loadingMovies ? (
+        {loadingMovies || loadingTrendingMovies ? (
           <ActivityIndicator size={"large"} color="#0000ff" className="mt-10 self-center" />
-        ): moviesError ? (
+        ): moviesError|| trendingMoviesError ? (
           <Text >
-            Error: {moviesError?.message}
+            Error: {moviesError?.message || trendingMoviesError?.message}
           </Text>
         ) : (
             <View className="mb-5 flex-1">
               <SearchBar onPress={() => router.push('/search')} 
               placeholder="Search for movies..."/>
+              {
+                trendingMovies && (
+                  <View className="mt-10">
+                    <Text className="text-lg text-white font-bold mb-3">
+                      Trending Movies
+                    </Text>
+                    <FlatList
+                      horizontal
+                      showsHorizontalScrollIndicator={false}
+                      ItemSeparatorComponent={() =>
+                        <View className="w-4">
+                        </View>
+                      }
+                      className="mb-4 mt-3"
+                      data={trendingMovies}
+                      keyExtractor={(item) => item.movie_id.toString()}
+                      renderItem={({item,index})=> (
+                        <TrendingCard movie={item} index={index} />
+                      )}/>
+                  </View>
+                )
+              }
               <>
                 <Text className="text-white font-bold mt-5 mb-3 text-lg">
                   Popular Movies
--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -26,15 +26,18 @@ const search = () => {
       const timeoutID = setTimeout(async () => {
         if (searchQuery.trim()) {
           await refetchMovies();
-          if( movies?.length > 0 && movies?.[0]) {
-            await updateSearchCount(searchQuery,movies[0]);
-          }
         } else {
           reset();
         }
       }, 500);
       return () => clearTimeout(timeoutID);
     },[searchQuery])
+
+    useEffect(() => {
+      if( movies?.length > 0 && movies?.[0]) {
+        updateSearchCount(searchQuery,movies[0]);
+      }
+    },[movies]);
   return (
     <View className='flex-1 bg-primary'>
       <Image
