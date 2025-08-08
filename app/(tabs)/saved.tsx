3b5974836f8d74e935cfffbea0c7650a6cd2bd2f
Mohit Raut <mohitraut.21@stvincentngp.edu.in>
2025-08-08 14:58:19 +0530

    feat: update profile and saved components to include images and enhance layout

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,10 +1,17 @@
-import { View, Text } from 'react-native'
+import { View, Text ,Image } from 'react-native'
 import React from 'react'
+import { icons } from '@/constants/icons'
 
 const saved = () => {
   return (
-    <View>
-      <Text>saved</Text>
+    <View className='flex-1 bg-primary px-10'>
+      <View className='flex justify-center items-center flex-1 flex-col gap-5'>
+        <Image
+          source={icons.save}
+          className='size-10'
+          tintColor='#fff'/>
+          <Text className="text-base text-gray-500 ">Saved</Text>
+      </View>
     </View>
   )
 }
